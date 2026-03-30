8cda1ccf2760000db0243b840db392b25610943d
Treinamento <klyciaiannesilvas@gmail.com>
2026-03-30 10:40:40 -0300

    inserir

--- src/main.py
+++ src/main.py
@@ -12,11 +12,18 @@ cursor = banco.cursor()
 
 cursor.execute("CREATE DATABASE IF NOT EXISTS anaclara_klycia")
 
-cursor.execute("CREATE TABLE USUARIO(id INT, username VARCHAR(255), email VARCHAR(50), senha VARCHAR(100), idade INT.)")
+# cursor.execute("CREATE TABLE USUARIO(id INT, username VARCHAR(255), email VARCHAR(50), senha VARCHAR(100), idade INT)")
 id_INT = int(input("id"))
-username = int(input("username"))
-email = int(input("email"))
-senha = int(input("senha"))
+username = input("username")
+email = input("email")
+senha = input("senha")
 idade = int(input("idade"))
-cursor.execute(f"INSERT INTO USUARIO VALUES ({id}, {username}, {email}, {senha}, {idade})")
+cursor.execute(f"INSERT INTO USUARIO VALUES ({id_INT}, '{username}', '{email}', '{senha}', {idade})")
 banco.commit()
+cursor.execute("SELECT * FROM USUARIO")
+
+dados = cursor.fetchall()
+
+# 5. Mostrar os dados
+for linha in dados:
+    print(linha)
